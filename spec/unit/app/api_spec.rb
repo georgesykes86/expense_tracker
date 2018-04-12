4697d6a6828a8b37cf4b6c8c59c6a318a99a27de
George Sykes <georgesykes86@gmail.com>
2018-04-12 09:58:31 +0100

    Updating sinatra gem and create parse_json helper

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -11,6 +11,10 @@ module ExpenseTracker
       API.new(ledger: ledger)
     end
 
+    def parse_json(json)
+      JSON.parse(json.body)
+    end
+
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
 
     describe 'POST /expenses' do
@@ -24,9 +28,7 @@ module ExpenseTracker
 
         it 'returns the expense id' do
           post '/expenses', JSON.generate(expense)
-
-          parsed = JSON.parse(last_response.body)
-          expect(parsed).to include('expense_id' => 417)
+          expect(parse_json(last_response)).to include('expense_id' => 417)
         end
 
         it 'responds with a 200 (ok)' do
@@ -47,9 +49,7 @@ module ExpenseTracker
 
         it 'returns an error message' do
           post '/expenses', JSON.generate(expense)
-
-          parsed = JSON.parse(last_response.body)
-          expect(parsed).to include('error' => 'Expense incomplete')
+          expect(parse_json(last_response)).to include('error' => 'Expense incomplete')
         end
 
         it 'responds with a 422 (Unprocessable entity)' do
