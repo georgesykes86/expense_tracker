ba28f3cf0a0f083d18ecfc2f033e60c02e409bf1
George Sykes <georgesykes86@gmail.com>
2018-04-12 09:36:36 +0100

    Added unit tests for failing and passing expense posts

--- app/api.rb
+++ app/api.rb
@@ -6,10 +6,18 @@ module ExpenseTracker
     def initialize(ledger: Ledger.new)
       @ledger = ledger
       super()
-    end 
+    end
 
     post '/expenses' do
-      JSON.generate('expense_id' => 42)
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)
+
+      if result.success?
+        JSON.generate('expense_id' => result.expense_id)
+      else
+        status 422
+        JSON.generate('error' => result.error_message)
+      end
     end
 
     get '/expenses/:date' do
