0d077559e6af44a252f33eefd3e77c3cfc7dc99d
George Sykes <georgesykes86@gmail.com>
2018-04-15 12:06:13 +0100

    Adding xml parsing functionality

--- app/api.rb
+++ app/api.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'json'
 require_relative 'ledger'
+require 'ox'
 
 module ExpenseTracker
   class API < Sinatra::Base
@@ -10,20 +11,43 @@ module ExpenseTracker
     end
 
     post '/expenses' do
-      expense = JSON.parse(request.body.read)
-      result = @ledger.record(expense)
+      if ['application/json', 'application/x-www-form-urlencoded'].include? request.media_type
+        expense = JSON.parse(request.body.read)
+        result = @ledger.record(expense)
 
-      if result.success?
-        JSON.generate('expense_id' => result.expense_id)
+        if result.success?
+          JSON.generate('expense_id' => result.expense_id)
+        else
+          status 422
+          JSON.generate('error' => result.error_message)
+        end
+      elsif request.media_type == 'text/xml'
+        expense = Ox.parse_obj(request.body.read)
+        result = @ledger.record(expense)
+
+        if result.success?
+          Ox.dump('expense_id' => result.expense_id)
+        else
+          status 422
+          Ox.dump('error' => result.error_message)
+        end
       else
         status 422
-        JSON.generate('error' => result.error_message)
+        return JSON.generate('error' => 'Unrecognised data format')
       end
     end
 
     get '/expenses/:date' do
-      result = @ledger.expenses_on(params['date']) || []
-      JSON.generate(result)
+      if request.accept? 'application/json'
+        result = @ledger.expenses_on(params['date']) || []
+        JSON.generate(result)
+      elsif request.accept? 'text/xml'
+        result = @ledger.expenses_on(params['date']) || []
+        result_json = JSON.generate(result)
+        Ox.dump(result_json)
+      else
+        JSON.generate('error' => 'Unrecognised data format')
+      end
     end
   end
 end
--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -16,6 +16,10 @@ module ExpenseTracker
       JSON.parse(json.body)
     end
 
+    def parse_xml(xml)
+      Ox.parse_obj(xml.body)
+    end
+
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
 
     describe 'POST /expenses' do
@@ -59,6 +63,66 @@ module ExpenseTracker
         end
       end
 
+      context 'when user submits a valid XML' do
+        let(:expense) { { 'some' => 'data' } }
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true,417,nil))
+        end
+
+
+        it 'responds with a 200 (ok)' do
+          header 'Content-Type', 'text/xml'
+          post '/expenses', Ox.dump(expense)
+          expect(last_response.status).to eq(200)
+        end
+
+        it 'returns the expense id' do
+          header 'Content-Type', 'text/xml'
+          post '/expenses', Ox.dump(expense)
+          expect(parse_xml(last_response)).to include('expense_id' => 417)
+        end
+
+      end
+
+      context 'when a user submits an invalid XML' do
+        let(:expense) { { 'some' => 'data' } }
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(false, 417, 'Expense incomplete'))
+        end
+
+        it 'returns an error message' do
+          header 'Content-Type', 'text/xml'
+          post '/expenses', Ox.dump(expense)
+          expect(parse_xml(last_response)).to include('error' => 'Expense incomplete')
+        end
+
+        it 'responds with a 422 (Unprocessable entity)' do
+          header 'Content-Type', 'text/xml'
+          post '/expenses', Ox.dump(expense)
+          expect(last_response.status).to eq(422)
+        end
+      end
+
+      context 'when a user submits an unrecognised data format' do
+        let(:expense) { { 'some' => 'data' } }
+
+        it 'returns an error message' do
+          header 'Content-Type', 'text/plain'
+          post '/expenses', expense.to_s
+          expect(parse_json(last_response)).to include('error' => 'Unrecognised data format')
+        end
+
+        it 'responds with a 422 (Unrecognised format)' do
+          header 'Content-Type', 'text/plain'
+          post '/expenses', expense.to_s
+          expect(last_response.status).to eq(422)
+        end
+      end
+
     end
 
     describe 'GET /expenses/:date' do
@@ -76,7 +140,7 @@ module ExpenseTracker
         end
         it 'responds with a 200 (OK)' do
           get "/expenses/#{date}"
-          expect(last_response.status).to eq (200)
+          expect(last_response.status).to eq(200)
         end
       end
 
@@ -95,7 +159,47 @@ module ExpenseTracker
 
         it 'responds with a 200 (OK)' do
           get "/expenses/#{date}"
-          expect(last_response.status).to eq (200)
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context 'when the user request xml format back' do
+        let(:date) {'2017-10-20'}
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with(date)
+            .and_return(Record.new("payee", 0.99, date))
+        end
+
+        it 'returns the expense record XML' do
+          header 'Accept', 'text/xml'
+          get "/expenses/#{date}"
+          expect(parse_xml(last_response)).not_to be_empty
+        end
+        it 'responds with a 200 (OK)' do
+          header 'Accept', 'text/xml'
+          get "/expenses/#{date}"
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context 'when the user request an unsupported format' do
+        let(:date) {'2017-10-20'}
+        before do
+          allow(ledger).to receive(:expenses_on)
+            .with(date)
+            .and_return(Record.new("payee", 0.99, date))
+        end
+
+        it 'returns the expense record in JSON' do
+          header 'Accept', 'text/plain'
+          get "/expenses/#{date}"
+          expect(parse_json(last_response)).to include('error' => 'Unrecognised data format')
+        end
+        it 'responds with a 200 (OK)' do
+          header 'Accept', 'text/plain'
+          get "/expenses/#{date}"
+          expect(last_response.status).to eq(200)
         end
       end
 
